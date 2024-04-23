c224fe5d4a6e205182f1c66275de2beb0700a3df
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-04-22 22:17:38 -0700

    StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,28 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
-} 
+ private ArrayList <Integer> steps;
+ private int limit;
+ public StepTracker(int setlimits){
+  limit = setlimits;
+ }
+ public int activeDays(){
+  int count = 0;
+  for(int i = 0; i < steps.size(); i++){
+   if(steps.get(i) >= limit){
+    count++;
+   }
+   return count;
+  }
+ }
+  public double averageSteps(){
+   int sum = 0;
+   for(int i = 0; i < steps.size(); i++){
+    sum += steps.get(i);
+   }
+   return sum/steps.size;
+  }
+  public int addDailySteps(int step){
+   steps.add(step);
+  }
+}
