927c94b22c0e33e17b00adc5e7e56085735f3619
Kyle Yuen <103063168+RandomHouseGuest@users.noreply.github.com>
2024-05-07 10:04:20 -0700

    StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -16,6 +16,9 @@ public class StepTracker
    return count;
  }
   public double averageSteps(){
+   if(steps.size() == 0){
+    return 0.0;
+   }
    int sum = 0;
    for(int i = 0; i < steps.size(); i++){
     sum += steps.get(i);
